d3f4d2202341d9952950f5ca1bb4327115591cee
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2020-08-19 01:24:34 -0300

    Deployed site

--- 01_messenger-like-app/src/components/MessagesList.js
+++ 01_messenger-like-app/src/components/MessagesList.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import FlipMove from "react-flip-move";
 import Message from "./Message";
 
