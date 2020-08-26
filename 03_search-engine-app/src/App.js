629634c538a7766159eb72b15c7f1eb6cc487281
Danak-UY <37196682+Danak-UY@users.noreply.github.com>
2020-08-26 19:49:23 -0300

    Create react app

---
+++ 03_search-engine-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "antd/dist/antd.css";
+import "./App.css";
+
+import Home from "./pages/Home";
+
+function App() {
+  return (
+    <div className="app">
+      <Home />
+    </div>
+  );
+}
+
+export default App;
---
+++ 03_search-engine-app/src/pages/Home.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "../assets/styles/Home.css";
+import { Button } from "antd";
+
+import Header from "../components/Header";
+
+function Home() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
+
+export default Home;
